63e6b23d26863cfac63144913a9678669356755f
Michael Zhu <65944154+mwzhu@users.noreply.github.com>
2020-08-25 23:41:53 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from main import recommend, unique_songs, print_songs
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    if request.method == "POST":
+        songs = list()
+        for i in range(1,4):
+            if request.form["song"+str(i)] is not None:
+                songs.append(request.form["song"+str(i)])
+        result = recommend(songs)
+        return render_template('recommend.html',  tables=[result.to_html(classes='data', header="true", index=False, justify='center')])
+
+    song_list = print_songs()
+    return render_template('home.html', song_list=song_list)
+
+@app.route("/songlist")
+def songlist():
+    result = unique_songs()
+    return render_template('songlist.html',  tables=[result.to_html(classes='data', header="true", justify='center')])
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,45 @@
+import pandas
+import numpy as np
+from sklearn.model_selection import train_test_split
+import Recommender as Recommender
+
+song_df_1 = pandas.read_table('user_profiles.txt',header=None)
+song_df_1.columns = ['user_id', 'song_id', 'listen_count']
+
+#Read song  metadata
+song_df_2 =  pandas.read_csv('song_data.csv')
+
+#Merge the two dataframes above to create input dataframe for recommender systems
+song_df = pandas.merge(song_df_1, song_df_2.drop_duplicates(['song_id']), on="song_id", how="left")
+song_df['song'] = song_df['title'].map(str) + " - " + song_df['artist_name']
+song_df = song_df.head(10000)
+
+#Code to get most popular songs, but implementation is too slow#
+# song_grouped = song_df.groupby(['song']).agg({'listen_count': 'count'}).reset_index()
+# popular_songs = song_grouped.sort_values(['listen_count', 'song'], ascending = [0,1])
+# popular_songs = popular_songs.head(1000)
+# song_df = song_df.loc[song_df['song'].isin(popular_songs['song'])]
+
+#Personalized song Recommender
+train_data, test_data = train_test_split(song_df, test_size = 0.20, random_state=0)
+
+is_model = Recommender.item_similarity_recommender_py()
+is_model.create(train_data, 'user_id', 'song')
+
+def recommend(songs):
+    return is_model.get_similar_items(songs)
+
+def unique_songs():
+    unique_songs = np.sort(song_df['song'].unique())
+    df = pandas.DataFrame(columns = ['Song'])
+    for i in range(len(unique_songs)):
+        df.loc[len(df)] = [unique_songs[i]]
+    df.index = np.arange(1, len(df)+1)
+    return df
+
+def print_songs():
+    list = []
+    unique_songs = np.sort(song_df['song'].unique())
+    for i in range(len(unique_songs)):
+        list.append(unique_songs[i])
+    return list
