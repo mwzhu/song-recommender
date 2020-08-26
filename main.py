ea63f92e96f0e65fd7b20e40b6297147fb4479ea
Michael Zhu <65944154+mwzhu@users.noreply.github.com>
2020-08-26 01:38:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import Recommender as Recommender
 
-song_df_1 = pandas.read_table('user_profiles.txt',header=None)
+song_df_1 = pandas.read_table('https://static.turi.com/datasets/millionsong/10000.txt',header=None)
 song_df_1.columns = ['user_id', 'song_id', 'listen_count']
 
 #Read song  metadata
-song_df_2 =  pandas.read_csv('song_data.csv')
+song_df_2 =  pandas.read_csv('https://static.turi.com/datasets/millionsong/song_data.csv')
 
 #Merge the two dataframes above to create input dataframe for recommender systems
 song_df = pandas.merge(song_df_1, song_df_2.drop_duplicates(['song_id']), on="song_id", how="left")
